8eb6ff003ee0c01b46ae6c8c0801177fcf1e8f20
Steven Helmis <steven.helmis@scitotec.de>
2013-02-05 15:29:22 +0100

    Intex Controller erweitert

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,27 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $this->_helper->viewRenderer->setNoRender();  
+        $this->_helper->layout()->disableLayout();  
+          
+        $objFront = Zend_Controller_Front::getInstance();  
+        $baseUrl = $objFront->getBaseUrl();  
+          
+        $server = new Zend_Json_Server();  
+        $server->setClass('Vqc_JsonRpc_Protokoll');  
+          
+        if ('GET' == $_SERVER['REQUEST_METHOD']) {  
+            $server->setTarget("http://127.0.0.1".$baseUrl."/api");  
+            $server->setEnvelope(Zend_Json_Server_Smd::ENV_JSONRPC_2);  
+              
+            $smd = $server->getServiceMap();  
+            header('Content-Type: application/json');  
+              
+            echo Zend_Json::prettyPrint($smd);  
+            return ;  
+        }  
+          
+        $server->handle();
     }
 
 
